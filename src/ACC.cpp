f0738729a5235e352027d9fc6ec629eb2aa0024c
docB <docb22@googlemail.com>
2022-06-25 20:00:20 +0200

    ACC Threshold

--- src/ACC.cpp
+++ src/ACC.cpp
@@ -3,10 +3,10 @@
 
 struct ACC : Module {
 	enum ParamId {
-		STEP_AMT_PARAM,SET_PARAM,RST_PARAM,PARAMS_LEN
+		STEP_AMT_PARAM,SET_PARAM,RST_PARAM,THRESHOLD_PARAM,PARAMS_LEN
 	};
 	enum InputId {
-    CLOCK_INPUT,RST_INPUT,STEP_AMT_INPUT,SET_INPUT,INPUTS_LEN
+    CLOCK_INPUT,RST_INPUT,STEP_AMT_INPUT,SET_INPUT,THRESHOLD_INPUT,INPUTS_LEN
 	};
 	enum OutputId {
 		CV_OUTPUT,TRIG_OUTPUT,OUTPUTS_LEN
@@ -25,11 +25,13 @@ struct ACC : Module {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
     configParam(STEP_AMT_PARAM,-1,1,0,"Step Amount");
     configParam(SET_PARAM,-10,10,0,"Set");
+    configParam(THRESHOLD_PARAM,-10,10,0,"Trigger Threshold");
     configButton(RST_PARAM,"Reset");
     configInput(CLOCK_INPUT,"Clock");
     configInput(RST_INPUT,"Reset");
     configInput(STEP_AMT_INPUT,"Step Amount");
     configInput(SET_INPUT,"Set");
+    configInput(THRESHOLD_INPUT,"Trigger Threshold");
     configOutput(TRIG_OUTPUT,"Trig");
     configOutput(CV_OUTPUT,"CV");
     divider.setDivision(32);
@@ -43,6 +45,9 @@ struct ACC : Module {
       if(inputs[STEP_AMT_INPUT].isConnected()) {
         getParamQuantity(STEP_AMT_PARAM)->setValue(clamp(inputs[STEP_AMT_INPUT].getVoltage(),-10.f,10.f)/10.f);
       }
+      if(inputs[THRESHOLD_INPUT].isConnected()) {
+        getParamQuantity(THRESHOLD_PARAM)->setValue(inputs[THRESHOLD_INPUT].getVoltage());
+      }
     }
     if(rstTrigger.process(inputs[RST_INPUT].getVoltage())|manualRstTrigger.process(params[RST_PARAM].getValue())) {
       rstPulse.trigger(0.001f);
@@ -51,12 +56,13 @@ struct ACC : Module {
     bool restGate=rstPulse.process(args.sampleTime);
     if(clockTrigger.process(inputs[CLOCK_INPUT].getVoltage()) & !restGate) {
         float amount=params[STEP_AMT_PARAM].getValue();
+        float threshold=params[THRESHOLD_PARAM].getValue();
         if(amount<0) {
-          if(state>0 && state+amount <=0) {
+          if(state>threshold && state+amount <=threshold) {
             trigPulse.trigger();
           }
         } else {
-          if(state<0 && state+amount >=0) {
+          if(state<threshold && state+amount >=threshold) {
             trigPulse.trigger();
           }
         }
@@ -73,20 +79,29 @@ struct ACCWidget : ModuleWidget {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/ACC.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-    float xpos=1.9f;
-    addInput(createInput<SmallPort>(mm2px(Vec(xpos,MHEIGHT-115.f)),module,ACC::CLOCK_INPUT));
-    addInput(createInput<SmallPort>(mm2px(Vec(xpos,MHEIGHT-103.f)),module,ACC::RST_INPUT));
-    addParam(createParam<MLEDM>(mm2px(Vec(xpos,MHEIGHT-95.0f)),module,ACC::RST_PARAM));
-    addParam(createParam<TrimbotWhite>(mm2px(Vec(xpos,MHEIGHT-81.0f)),module,ACC::SET_PARAM));
-    addInput(createInput<SmallPort>(mm2px(Vec(xpos,MHEIGHT-73.f)),module,ACC::SET_INPUT));
-    addParam(createParam<TrimbotWhite>(mm2px(Vec(xpos,MHEIGHT-61.0f)),module,ACC::STEP_AMT_PARAM));
-    addInput(createInput<SmallPort>(mm2px(Vec(xpos,MHEIGHT-53.f)),module,ACC::STEP_AMT_INPUT));
-    addOutput(createOutput<SmallPort>(mm2px(Vec(xpos,MHEIGHT-31.f)),module,ACC::TRIG_OUTPUT));
-    addOutput(createOutput<SmallPort>(mm2px(Vec(xpos,MHEIGHT-19.f)),module,ACC::CV_OUTPUT));
+    float x=1.9f;
+    float y=10;
+    addInput(createInput<SmallPort>(mm2px(Vec(x,y)),module,ACC::CLOCK_INPUT));
+    y+=12;
+    addInput(createInput<SmallPort>(mm2px(Vec(x,y)),module,ACC::RST_INPUT));
+    y+=8;
+    addParam(createParam<MLEDM>(mm2px(Vec(x,y)),module,ACC::RST_PARAM));
+    y+=12;
+    addParam(createParam<TrimbotWhite>(mm2px(Vec(x,y)),module,ACC::SET_PARAM));
+    y+=8;
+    addInput(createInput<SmallPort>(mm2px(Vec(x,y)),module,ACC::SET_INPUT));
+    y+=12;
+    addParam(createParam<TrimbotWhite>(mm2px(Vec(x,y)),module,ACC::STEP_AMT_PARAM));
+    y+=8;
+    addInput(createInput<SmallPort>(mm2px(Vec(x,y)),module,ACC::STEP_AMT_INPUT));
+    y+=12;
+    addParam(createParam<TrimbotWhite>(mm2px(Vec(x,y)),module,ACC::THRESHOLD_PARAM));
+    y+=8;
+    addInput(createInput<SmallPort>(mm2px(Vec(x,y)),module,ACC::THRESHOLD_INPUT));
+    y+=12;
+    addOutput(createOutput<SmallPort>(mm2px(Vec(x,y)),module,ACC::TRIG_OUTPUT));
+    y+=12;
+    addOutput(createOutput<SmallPort>(mm2px(Vec(x,y)),module,ACC::CV_OUTPUT));
   }
 };
 
